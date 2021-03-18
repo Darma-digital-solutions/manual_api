3dcd482ab5fb00ea54251cb5857c1152839ba5f9
iamJune20 <dikirahmadsandi@outlook.com>
2021-03-18 18:13:17 +0700

    Tidak bisa terima request body | Insert nya aman

--- routes/router.go
+++ routes/router.go
@@ -2,18 +2,18 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/iamJune20/dds/controllers"
+	"github.com/iamJune20/dds/src/controllers"
 )
 
 func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/apps", controllers.GetAllApp).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/buku/{id}", controller.AmbilBuku).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/buku", controller.TmbhBuku).Methods("POST", "OPTIONS")
-	// router.HandleFunc("/api/buku/{id}", controller.UpdateBuku).Methods("PUT", "OPTIONS")
-	// router.HandleFunc("/api/buku/{id}", controller.HapusBuku).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/api/apps", controllers.GetApps).Methods("GET")
+	router.HandleFunc("/api/app/{code}", controllers.GetApp).Methods("GET")
+	router.HandleFunc("/api/app", controllers.InsertApp).Methods("POST")
+	// router.HandleFunc("/api/buku/{id}", controller.UpdateBuku).Methods("PUT")
+	// router.HandleFunc("/api/buku/{id}", controller.HapusBuku).Methods("DELETE")
 
 	return router
 }
