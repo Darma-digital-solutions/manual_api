349509ad7dd0ec1bcb6caae66f927e718d4640d9
iamJune20 <dikirahmadsandi@outlook.com>
2021-03-29 13:58:18 +0700

    InsyaAllah

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/iamJune20/dds
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
--- routes/router.go
+++ routes/router.go
@@ -1,44 +1,83 @@
 package routes
 
 import (
+	"net/http"
+
+	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/gorilla/mux"
 	"github.com/iamJune20/dds/src/controllers"
 )
 
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bearer := r.Header.Get("Authorization")
+		errBearer := validation.Validate(bearer,
+			validation.Required,
+		)
+		if errBearer != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			controllers.ReturnError(w, 401, "SignIn terlebih dahulu")
+			return
+		}
+		// fmt.Printf("Bearer = %v ", tokenString)
+		var cek = controllers.ValidateToken(bearer)
+		if cek == "" {
+			next.ServeHTTP(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			controllers.ReturnError(w, 401, cek)
+			return
+		}
+
+	})
+}
+
 func Router() *mux.Router {
 
 	router := mux.NewRouter()
+	api := router.PathPrefix("/api").Subrouter()
+	api.Use(middlewareOne)
+	router.HandleFunc("/public/signin", controllers.Signin).Methods("POST")
+	router.HandleFunc("/public/refresh", controllers.Refresh).Methods("POST")
 
-	router.HandleFunc("/api/apps", controllers.GetApps).Methods("GET")
-	router.HandleFunc("/api/app/{code}", controllers.GetApp).Methods("GET")
-	router.HandleFunc("/api/app", controllers.InsertApp).Methods("POST")
-	router.HandleFunc("/api/app/{code}", controllers.UpdateApp).Methods("PUT")
-	router.HandleFunc("/api/app/{code}", controllers.DeleteApp).Methods("DELETE")
+	api.HandleFunc("/apps", controllers.GetApps).Methods("GET")
+	// router.HandleFunc("/public/apps", controllers.GetApps).Methods("GET")
+	router.HandleFunc("/public/app/{code}", controllers.GetApp).Methods("GET")
 
-	router.HandleFunc("/api/categories", controllers.GetCategories).Methods("GET")
-	router.HandleFunc("/api/categoriesByApp/{code}", controllers.GetCategoriesByAppCode).Methods("GET")
-	router.HandleFunc("/api/category/{code}", controllers.GetCategory).Methods("GET")
-	router.HandleFunc("/api/category/{app_code}", controllers.InsertCategory).Methods("POST")
-	router.HandleFunc("/api/category/{code}", controllers.UpdateCategory).Methods("PUT")
-	router.HandleFunc("/api/category/{code}", controllers.DeleteCategory).Methods("DELETE")
+	// with auth
+	api.HandleFunc("/app", controllers.InsertApp).Methods("POST")
+	api.HandleFunc("/app/{code}", controllers.UpdateApp).Methods("PUT")
+	api.HandleFunc("/app/{code}", controllers.DeleteApp).Methods("DELETE")
 
-	router.HandleFunc("/api/manuals", controllers.GetManuals).Methods("GET")
-	router.HandleFunc("/api/manualsByApp/{code}", controllers.GetManualsByAppCode).Methods("GET")
-	router.HandleFunc("/api/manual/{code}", controllers.GetManual).Methods("GET")
-	router.HandleFunc("/api/manual/{app_code}", controllers.InsertManual).Methods("POST")
-	router.HandleFunc("/api/manual/{code}", controllers.UpdateManual).Methods("PUT")
-	router.HandleFunc("/api/manual/{code}", controllers.DeleteManual).Methods("DELETE")
+	router.HandleFunc("/public/categories", controllers.GetCategories).Methods("GET")
+	router.HandleFunc("/public/categoriesByApp/{code}", controllers.GetCategoriesByAppCode).Methods("GET")
+	router.HandleFunc("/public/category/{code}", controllers.GetCategory).Methods("GET")
 
-	router.HandleFunc("/api/contents", controllers.GetContents).Methods("GET")
-	router.HandleFunc("/api/content/{code}", controllers.GetContent).Methods("GET")
-	router.HandleFunc("/api/content/{manual_code}/{category_code}", controllers.InsertContent).Methods("POST")
-	router.HandleFunc("/api/content/{code}", controllers.UpdateContent).Methods("PUT")
-	router.HandleFunc("/api/content/{code}", controllers.DeleteContent).Methods("DELETE")
-	router.HandleFunc("/api/contentsIn/{manual_code}/{category_code}", controllers.GetContentInManualAndCategory).Methods("GET")
+	// with auth
+	api.HandleFunc("/category/{app_code}", controllers.InsertCategory).Methods("POST")
+	api.HandleFunc("/category/{code}", controllers.UpdateCategory).Methods("PUT")
+	api.HandleFunc("/category/{code}", controllers.DeleteCategory).Methods("DELETE")
 
-	router.HandleFunc("/api/searchAll", controllers.GetSearchAll).Methods("POST")
-	router.HandleFunc("/api/searchOne/{app_code}", controllers.GetSearchOne).Methods("POST")
+	router.HandleFunc("/public/manuals", controllers.GetManuals).Methods("GET")
+	router.HandleFunc("/public/manualsByApp/{code}", controllers.GetManualsByAppCode).Methods("GET")
+	router.HandleFunc("/public/manual/{code}", controllers.GetManual).Methods("GET")
 
-	// GetSearchAll
+	// with auth
+	api.HandleFunc("/manual/{app_code}", controllers.InsertManual).Methods("POST")
+	api.HandleFunc("/manual/{code}", controllers.UpdateManual).Methods("PUT")
+	api.HandleFunc("/manual/{code}", controllers.DeleteManual).Methods("DELETE")
+
+	router.HandleFunc("/public/contents", controllers.GetContents).Methods("GET")
+	router.HandleFunc("/public/content/{code}", controllers.GetContent).Methods("GET")
+	router.HandleFunc("/public/contentsIn/{manual_code}/{category_code}", controllers.GetContentInManualAndCategory).Methods("GET")
+
+	// with auth
+	api.HandleFunc("/content/{manual_code}/{category_code}", controllers.InsertContent).Methods("POST")
+	api.HandleFunc("/content/{code}", controllers.UpdateContent).Methods("PUT")
+	api.HandleFunc("/content/{code}", controllers.DeleteContent).Methods("DELETE")
+
+	api.HandleFunc("/searchAll", controllers.GetSearchAll).Methods("POST")
+	api.HandleFunc("/searchOne/{app_code}", controllers.GetSearchOne).Methods("POST")
+
 	return router
 }
